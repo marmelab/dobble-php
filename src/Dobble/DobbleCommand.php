651c0438605bc0f14d040ec075b1b646fe02d296
Kmaschta <maschtaler.kevin.iris@gmail.com>
2015-10-30 14:38:33 +0100

    Algo final version

--- src/Dobble/DobbleCommand.php
+++ src/Dobble/DobbleCommand.php
@@ -5,6 +5,7 @@ namespace Marmelab\Dobble;
 use Symfony\Component\Console\Command\Command;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Input\InputArgument;
+use Symfony\Component\Console\Input\InputOption;
 use Symfony\Component\Console\Output\OutputInterface;
 
 class DobbleCommand extends Command
@@ -19,32 +20,56 @@ class DobbleCommand extends Command
             InputArgument::REQUIRED,
             'Number of elements per card'
         );
+
+        $this->addOption(
+            'mini',
+            null,
+            InputOption::VALUE_NONE,
+            'If specified, fetch the minimum card for given elements per card'
+        );
+
+        $this->addOption(
+            'hide-cards',
+            'c',
+            InputOption::VALUE_NONE,
+            'Hide cards for better lisibility'
+        );
     }
 
     protected function execute(InputInterface $input, OutputInterface $output)
     {
         $nbElements = $input->getArgument('elements_per_card');
+        $mini = $input->getOption('mini');
 
-        if ($nbElements <= 0) {
-            $output->writeln('<error>Number of elements must be a positive integer</error>');
+        if ($nbElements <= 1) {
+            $output->writeln('<error>Number of elements must be a positive integer and greater than one</error>');
 
             return 1;
         }
 
         try {
-            $generator = new DeckGenerator($nbElements);
+            $mode = ($mini) ? DeckGenerator::ALGO_MINI : DeckGenerator::ALGO_DEFAULT;
+            $generator = new DeckGenerator($nbElements, $mode);
             $deck = $generator->generate();
         } catch (DobbleException $e) {
-            $output->writeln(sprintf('Error: <error>%s</error>', $e->getMessage()));
+            if (!$mini) {
+                $output->writeln('<comment>Unable to generate with default mode. Trying now with --mini mode</comment>');
+                $generator = new DeckGenerator($nbElements, DeckGenerator::ALGO_MINI);
+                $deck = $generator->generate();
+            } else {
+                $output->writeln(sprintf('Error: <error>%s</error>', $e->getMessage()));
 
-            return 1;
+                return 1;
+            }
         }
 
         $output->writeln(sprintf('Elements per card: <info>%d</info>', $nbElements));
         $output->writeln(sprintf('Deck generated with <info>%d</info> cards', count($deck)));
-        $output->writeln('Cards :');
-        foreach ($deck->getCards() as $card) {
-            $output->writeln(sprintf('- %s', $card));
+        if (!$input->getOption('hide-cards')) {
+            $output->writeln('Cards :');
+            foreach ($deck->getCards() as $card) {
+                $output->writeln(sprintf('- %s', $card));
+            }
         }
         if ($deck->validate()) {
             $output->writeln('Deck is valid: <info>true</info>');
